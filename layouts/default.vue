af43edf39d30930d7472dea241b766785368a698
Ned <ed.hask85@gmail.com>
2023-09-13 16:45:26 -0400

    Added eslint and tailwind css (eslint still not quite right)

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<header class="shadow-sm bg-white">
+			<nav class="container mx-auto p-4 flex justify-between">
+				<NuxtLink class="font-bold" to='/'>Nuxt Dojo</NuxtLink>
+				<ul class="flex gap-4">
+					<li><NuxtLink to='/'>Home</NuxtLink></li>
+					<li><NuxtLink to='/about'>About</NuxtLink></li>
+					<li><NuxtLink to='/products' class="btn">Products</NuxtLink></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+
+
+	<!-- output the page content-->
+	<div class="container mx-auto p-4">
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+	.router-link-exact-active {
+		color: red;
+	}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   
-  devtools: { enabled: true },
+  // devtools: { enabled: true },
+
+  modules: [
+    // Simple usage
+    // '@nuxtjs/eslint-module', 
+    '@nuxtjs/tailwindcss'
+
+  ],
+  vue: {
+    compilerOptions: {
+      //treat all tags with a dash as custom elements
+      isCustomElement: (tag) => tag.includes('-'),
+
+    }
+  },
 
 })
