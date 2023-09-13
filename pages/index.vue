96169003aa1e5a5e2bdd06d1e6e9e541c655ff46
Ned <ed.hask85@gmail.com>
2023-09-12 22:32:37 -0400

    Added products page & Sass and esbuild to package.json

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<h2>This is the home page!</h2>
+		<p>Overhaul aye Spanish Main booty bilge bucko sheet draught lee lugger. Pirate brig careen coffer man-of-war tackle six pounders to go on account Brethren of the Coast wherry. Trysail avast line run a shot across the bow bowsprit bounty flogging cutlass wherry grapple. Hempen halter keelhaul Gold Road knave Arr matey long boat jib to go on account overhaul.</p>
+</template>
+
+<style lang='scss' scoped>
+	
+		h2 {
+			color: green;
+			font-size: 3rem;
+		}
+
+
+		p {
+			font-size: 2rem;
+		}
+		
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<h2>The product page.</h2>
+		<p>Topmast fluke gangway Corsair smartly port ye quarterdeck measured fer yer chains sheet. Jolly boat galleon pillage aft bring a spring upon her cable keel avast spyglass boatswain belaying pin. Run a shot across the bow code of conduct wherry transom broadside gun fire ship Brethren of the Coast plunder Admiral of the Black. Lee jack hang the jib Pirate Round parrel starboard Buccaneer Davy Jones' Locker Spanish Main spirits.
+
+Main sheet grapple snow take a caulk starboard quarterdeck Sail ho Davy Jones' Locker reef scuppers. Interloper Cat o'nine tails mizzen Blimey spike warp hempen halter fluke yardarm swab. Barque lanyard bucko spanker hail-shot lugsail salmagundi warp coxswain dance the hempen jig. Mizzenmast gaff gibbet gabion wherry fathom man-of-war landlubber or just lubber furl draft.
+</p>
+
+</template>
+
+<style lang='scss' scoped>
+
+
+h2 {
+	color: green;
+	font-size: 3rem;
+}
+
+p {
+	color: blue;
+}
+
+</style>
