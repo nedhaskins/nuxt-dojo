1db020cd8cf2881c715c9e13fe349b04130d3f9a
Ned <ed.hask85@gmail.com>
2023-09-15 16:04:42 -0400

    Added <ProductDetails> component

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,26 +1,21 @@
+<template>
+	<div>
+		<ProductDetails :product="product" />
+	</div>
+</template>
+
 <script setup>
 	const { id } = useRoute().params;
 	const uri = 'https://fakestoreapi.com/products/' + id;
 
 	// fetch the product
-
-	const { data: product } = await useFetch(uri);
+	const { data: product } = await useFetch(uri, { key: id});
 
 	definePageMeta({
 		layout: 'products',
 	})
 
 </script>
-
-<template>
-	<div>
-		<p>{{ product.title }}</p>
-		<p>{{ product.price }}</p>
-		<p>{{ product.id }}</p>
-	</div>
-</template>
-
-
 
 <style lang='scss' scoped>
 	
